80523f8e7746dd44e7baf1da9c40c642a4ab4095
Sunny4826 <118628971+Luna45123@users.noreply.github.com>
2024-11-14 14:41:13 +0700

    change package

--- src/main/java/com/Moodface.java
+++ src/main/java/com/Moodface.java
@@ -1,4 +1,4 @@
-package com.hackathon.hackathon;
+package com;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
--- src/main/java/com/hackathon/AppConfig.java
+++ src/main/java/com/hackathon/AppConfig.java
@@ -1,4 +1,4 @@
-package com.hackathon.hackathon;
+package com.hackathon;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
